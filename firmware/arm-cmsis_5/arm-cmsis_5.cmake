d6dac72b6a902a4769061e9e3f725fb491fca0a4
Craig Hutchinson <54269136+CraigHutchinson@users.noreply.github.com>
2021-02-19 15:32:38 +0000

    WIP support fo Microchip DFP and Arm CMSIS Cmake MOdules

---
+++ firmware/arm-cmsis_5/arm-cmsis_5.cmake
@@ -0,0 +1,51 @@
+# This Cmake will be injected into the root folder of FetchCOntent for https://github.com/ARM-software/CMSIS_5
+cmake_minimum_required (VERSION 3.18)
+
+project( arm-cmsis_5 LANGUAGES C ASM)
+
+# MCU can be any of: samd51g19a,samd51j18a,samd51j19a,samd51j20a,samd51n19a,samd51n20a,samd51p19a,samd51p20a
+set(MCU "samd51j19a" CACHE STRING "samd51::csp MCU to build against")
+set_property(CACHE MCU PROPERTY STRINGS 
+    samd51g18a 
+    samd51g19a 
+    samd51j18a 
+    samd51j19a
+    samd51j20a
+    samd51n19a
+    samd51n20a
+    samd51p19a
+    samd51p20a )
+
+set(MCURunsFrom "flash" CACHE STRING "Atmel Samd51 MCU executes add_compile_definitions from flash or ram")
+set_property(CACHE MCURunsFrom PROPERTY STRINGS 
+    flash 
+    ram
+)
+
+add_library(arm-cmsis_5)
+set_target_properties( arm-cmsis_5 PROPERTIES
+    C_STANDARD 11
+)
+
+target_include_directories( arm-cmsis_5
+    PUBLIC
+        ${CMAKE_CURRENT_LIST_DIR}/CMSIS/Core/Include/
+        ${CMAKE_CURRENT_LIST_DIR}/CMSIS/CoreValidation/Include/
+        ${CMAKE_CURRENT_LIST_DIR}/CMSIS/DSP/Include/
+        ${CMAKE_CURRENT_LIST_DIR}/CMSIS/Driver/Include/
+    PRIVATE
+         $<$<CXX_COMPILER_ID:GNU>: ${CMAKE_CURRENT_LIST_DIR}/CMSIS/Lib/GCC/>
+)
+
+target_sources( arm-cmsis_5 
+    PRIVATE
+         $<$<CXX_COMPILER_ID:GNU>: ${CMAKE_CURRENT_LIST_DIR}/Device/ARM/ARMCM4/Source/GCC/startup_ARMCM4.S>
+        # $<$<CXX_COMPILER_ID:GNU>: ${CMAKE_CURRENT_LIST_DIR}/Device/ARM/ARMCM4/Source/GCC/gcc_arm.ld>
+) 
+
+target_link_options( arm-cmsis_5  PRIVATE 
+	-L${nRF5_SDK_ROOT}/modules/nrfx/mdk
+	-T${CMAKE_CURRENT_LIST_DIR}/gcc_nrf52.ld
+)
+
+# TODO: -L{runtime.tools.CMSIS-5.4.0.path}/CMSIS/Lib/GCC/"  -larm_cortexM4lf_math.a
